f6bde2d80f90d2a9b546291ef3333f8c7dd17cb6
Brenton Horne <brentonhorne77@gmail.com>
2018-12-30 20:45:16 +1000

    Rm url-quote-magic

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -8,8 +8,8 @@ if [[ $ZSH_VERSION != 5.1.1 ]]; then
   			autoload -Uz bracketed-paste-magic
   			zle -N bracketed-paste bracketed-paste-magic
   		fi
-  		autoload -Uz url-quote-magic
-  		zle -N self-insert url-quote-magic
+#  		autoload -Uz url-quote-magic
+#  		zle -N self-insert url-quote-magic
       break
   	fi
   done
